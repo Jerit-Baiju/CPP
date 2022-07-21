dd7989c0863a2fd0b61238d3d4e55c823f2bf2d6
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:35:25 +0530

    short hand if else

---
+++ short_if_else.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int time = 15;
+    string result = (time > 18) ? "Good Day" : "Good Evening";
+    cout << result;
+    return 0;
+}
