c7fbaa57d7b975c862371b65c2b306d2ea718e7f
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:19:12 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 200 + 100;
+    int y = 200 - 4;
+    int z = 300 / 3;
+    cout << z;
+    return 0;
+    // % = modulus
+    // ++ increment
+    // -- decrement
+}
