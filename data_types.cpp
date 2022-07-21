da888b04be782b63fd7a7d90725aed054a8ff19b
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:13:46 +0530

    data types

---
+++ data_types.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    float float_num = 5.09;
+    double double_num = 8.99;
+    char my_letter = 'o';
+    bool my_bool = true;
+    string text = "hello";
+    cout << text;
+}
