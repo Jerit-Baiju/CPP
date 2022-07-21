9d534c8c770e87548914252b110ab88e22d5a018
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:51:13 +0530

    do while loop

---
+++ do_while.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    do
+    {
+        cout << i << "\n";
+        i++;
+    } while (i < 5);
+    return 0;
+}
