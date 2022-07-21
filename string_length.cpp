c22d1bc52fad80982de4e91463dbca59f8d3b3d0
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:22:10 +0530

    string lenth

---
+++ string_length.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string text = "Jerit Baiju";
+    cout << "the length of the text is > " << text.length();
+
+    return 0;
+}
