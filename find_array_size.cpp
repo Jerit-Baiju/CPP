2888a41f1c0951c641a149a319fee15d1ebca09e
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 18:21:06 +0530

    arrays

---
+++ find_array_size.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[] = {'a', 'b', 'c', 'd'};
+    int length = sizeof(arr);
+    cout << length ;
+
+    return 0;
+}
+string
