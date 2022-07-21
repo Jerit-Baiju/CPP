7909c2d3781a010c3df21349f7ad32fcf781aaec
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 14:25:46 +0530

    user input

---
+++ user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    cout << "type anything > ";
+    cin >> x;
+    cout << "you have typed " << x ;
+    return 0;
+}
