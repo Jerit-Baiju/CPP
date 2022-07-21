db7e96eae5f4b37ed90ee86f5c2dc2596fc0be0a
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 16:18:09 +0530

    loop through array

---
+++ loop_through_array.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (int i = 0; i < 4; i++)
+    {
+        cout << i << " : " << cars[i] << "\n";
+    }
+    return 0;
+}
