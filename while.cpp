05a8b4ffbc9e22305b8380022bf278d90a425308
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:48:17 +0530

    while

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while (i < 5)
+    {
+        cout << i << "\n";
+        i++;
+    }
+    return 0;
+}
