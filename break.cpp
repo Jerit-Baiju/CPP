707996034916874fbf9f221c40e9334b49e3838c
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-21 15:55:43 +0530

    break

---
+++ break.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // break
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 4)
+        {
+            break;
+        }
+        cout << i << "\n";
+    }
+    return 0;
+}
