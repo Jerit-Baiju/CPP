8d6218728b6881421429801d0afbf61862525505
Jerit Baiju <jeritalumkal@gmail.com>
2022-12-03 18:23:52 +0530

    .

--- arrays.cpp
+++ arrays.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
     int nums[10] = {7, 4, 2, 96, 8, 41, 52, 5, 58, 23};
-    cout << nums[1];
+    for (int i = 0; i < 10; i++)
+    {
+        cout <<  nums[i];
+    }
+    
     return 0;
 }
